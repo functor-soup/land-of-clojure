8df1b4445631443607eed86f3e4cb6e36ea8c29d
luke <nerorevenge@gmail.com>
2016-06-01 01:57:45 -0400

    5th chapter  almost done

--- chapter5/textgameengine.clj
+++ chapter5/textgameengine.clj
@@ -12,7 +12,55 @@
               [["living-room" "east" "door"]]
               [["living-room" "downstairs" "ladder"]]]))
 
-(defn describe-path (edge)
-  (str "there is a " (edge 2) " going " (edge 1) " from here."))
+(def *objects* ["whiskey" "bucket" "frog" "chain"])
 
-(describe-path (*edges*))
+(def ^:dynamic *object-locations* (zipmap *objects* ["living-room"
+                                           "living-room"
+                                           "garden"
+                                           "garden"]))
+
+(defn describe-location [location nodes]
+  (nodes location))
+
+
+(defn describe-path [edge]
+  (str " There is a " (edge 2) " going " (edge 1) " from here."))
+
+(defn describe-paths [location edges]
+   (apply str (map #(describe-path %1) (edges location))))
+
+(defn objects-at [loc objs obj-locs]
+  (let [at-loc-p #(= (obj-locs %) loc)]
+    (filter at-loc-p objs)
+    ))
+
+
+(defn describe-objects [loc objs obj-locs]
+  (let [describe-obj #(str " you see a " % " on the floor.")]
+    (apply str (map describe-obj (objects-at loc objs obj-locs)))))
+
+(defn look [location]
+  (println (apply str
+    (interleave [(describe-location location *nodes*)
+                 (describe-paths location *edges*)
+                 (describe-objects location *objects* *object-locations*)]
+                 (repeat "\n")))))
+
+
+;; the following function deviates
+;; from the book in the sense that this
+;; function does not modify any global state
+;; (probably need to refactor this)
+(defn walk [location direction]
+  (let [next (first (filter #(= (% 1) direction) (*edges* location)))]
+    (if next
+      (do
+        (look (next 0))
+        (next 0))
+      (println "you cannot go that way"))))
+
+;; the non-mutative part of this will eventually make
+;; this complicated in the next chapter yay!!!
+(defn pickup [location object]
+  (if (objects-at location *objects* *object-locations*)))
+(walk  "living-room" "west")
