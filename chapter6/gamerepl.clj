80592f696ade4796dd23b9fb0b54482d29488b32
luke <nerorevenge@gmail.com>
2016-06-03 20:07:52 -0400

    Transfer to the other computer

--- chapter6/gamerepl.clj
+++ chapter6/gamerepl.clj
@@ -1,3 +1,18 @@
-(load-file  "../chapter5/textgameengine.clj") 
+(require '[clojure.string :as str])
+(load-file  "../chapter5/textgameengine.clj")
+
+(def *allowed-commands* ["look" "walk"
+                          "pickup" "inventory"])
+
+
+(defn game-read
+  []
+  (let [cmd (read-line)]))
+
+
+(defn game-eval
+  [sexp]
+  (let [command ]
+    (if () (eval))))
 
 (look "garden")
