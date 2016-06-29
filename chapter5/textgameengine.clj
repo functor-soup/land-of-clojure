1a4957245497146704c9aeb372f3704a336dc80d
luke <nerorevenge@gmail.com>
2016-06-29 16:07:19 -0400

    chapter 5 refactor

--- chapter5/textgameengine.clj
+++ chapter5/textgameengine.clj
@@ -1,81 +1,75 @@
 (def nodes ["living-room" "garden" "attic"])
 
-(def *nodes* (zipmap
-              nodes
-              ["you are in the living-room. a wizard is snoring loudly on the couch"
-               "you are in a beautiful garden. there is a well in front of you"
-               "you are in the attic. there is a giant welding torch in the corner"
-               ]))
+(def *nodes*
+  {
+    "living-room"  "you are in the living-room. a wizard is snoring loudly on the couch"
+    "garden"       "you are in a beautiful garden. there is a well in front of you"
+    "attic"        "you are in the attic. there is a giant welding torch in the corner"
+  })
 
-(def *edges* (zipmap nodes
-             [[["garden" "west" "door"] ["attic" "upstairs" "ladder"]]
-              [["living-room" "east" "door"]]
-              [["living-room" "downstairs" "ladder"]]]))
+(def *edges*
+  {
+    "living-room" {"garden" ["west" "door"] "attic" ["upstairs" "ladder"]}
+    "garden"      {"living-room" ["east" "door"]}
+    "attic"       {"living-room" ["downstairs" "ladder"]}
+   })
 
 (def *objects* ["whiskey" "bucket" "frog" "chain"])
 
-(def *object-locations* (atom (zipmap *objects* ["living-room"
-                                             "living-room"
-                                              "garden"
-                                              "garden"])))
+(def *object-locations* (atom {"living-room" ["whiskey" "bucket"]
+                               "garden"      ["frog" "chain"]
+                               "body"        [] }))
 
+;; current location
 (def *location* (atom "living-room"))
 
 (defn describe-location [location nodes]
   (nodes location))
 
-
 (defn describe-path [edge]
-  (str " There is a " (edge 2) " going " (edge 1) " from here."))
+  (let [[direction object] (edge 1)]
+  (str " There is a " object " going " direction " from here.")))
 
 (defn describe-paths [location edges]
-   (apply str (map #(describe-path %1) (edges location))))
+   (apply str (map #(describe-path %) (edges location))))
 
-(defn objects-at [loc objs obj-locs]
-  (let [at-loc-p #(= (obj-locs %) loc)]
-    (filter at-loc-p objs)
-    ))
-
-
-(defn describe-objects [loc objs obj-locs]
+(defn describe-objects [loc obj-locs]
   (let [describe-obj #(str " you see a " % " on the floor.")]
-    (apply str (map describe-obj (objects-at loc objs obj-locs)))))
+    (apply str (map describe-obj (get obj-locs loc)))))
 
 (defn look []
    (apply str
     (interleave [(describe-location @*location* *nodes*)
                  (describe-paths @*location* *edges*)
-                 (describe-objects @*location* *objects* @*object-locations*)]
+                 (describe-objects @*location* @*object-locations*)]
                  (repeat "\n"))))
 
 (defn walk [direction]
-  (let [next (first (filter #(= (% 1) direction) (*edges* @*location*)))]
+  (let [values (get *edges* @*location*)
+        keys_  (keys values)
+        vals_  (->> (vals values)
+                    (map first))
+        new-map (zipmap vals_ keys_)
+        next (get new-map direction)]
     (if next
       (do
-        (reset! *location* (next 0))
+        (reset! *location* next)
         (look))
       "you cannot go that way")))
 
-;; if () evaluates to true
-;; but (true? ()) evaluates to false 
+;; seems messy, doubt any future improvements
 (defn pickup [object]
-  (if (seq (filter #(= object %) (objects-at @*location* *objects* @*object-locations*)))
+  (if (some #(= object %) (get @*object-locations*  @*location*))
     (do
-      (swap! *object-locations* assoc object "body")
+      (swap! *object-locations* update-in ["body"] #(concat % [object]))
       (str "you are now carrying the " object))
     (str "you cannot get that")))
 
 
 (defn inventory []
-  (str "inventory :" (apply str (-> "body"
-                                        (objects-at  *objects* @*object-locations*)
-                                        (interleave (repeat " , "))
-                                        (butlast)))))
+  (str "inventory :"
+       (apply str (-> (get @*object-locations* "body")
+                      (interleave (repeat " , "))
+                      (butlast)))))
 
 
-;; just for tests
-;; move along .. move along
-;; (look "garden")
-;; (pickup  "garden" "frog")
-;; (pickup  "garden" "chain")
-;; (inventory)
